1767651b98909e2d1cecb4cd2227422111df5057
Sebas Sebas <seplarui@gmail.com>
2017-01-15 22:39:05 +0100

    Ejemplo

--- Pruebas/Asignaturas/Modelo.php
+++ Pruebas/Asignaturas/Modelo.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 include_once("Config.php");
 include_once("Profesor.php");
 
@@ -19,7 +17,34 @@ class Modelo {
     
     public function setProfesor($profesor) {
         
-        $consulta='INSERT INTO profesor(nombre) VALUES("'$profesor->getNombre().'");';
+       $consulta = 'INSERT INTO profesor (nombre) VALUES ("' .$profesor->getNombre() . '");';
+       
+       echo $consulta."<br/>";
+       
+       $result=$this->conexion->prepare($consulta);
+       $count=$result->execute(array(":nombre"=>$profesor->getNombre()));
+       
+       if ($count==1) {
+           return true;
+       }else {
+           return false;
+       }
+               
+    }
+    
+    public function getProfesores() {
+        $consulta="SELECT * FROM profesor";
+        $result=$this->conexion->query($consulta);
+        
+        $profesores=array();
+        $cont=0;
+        
+        while($fila=$result->fetch()) {
+            $profesor=new Profesor($fila["id"],$fila["nombre"]);
+            $profesores[$cont]=$profesor;
+            $cont++;
+        }
+        return $profesores;
     }
 }
 
